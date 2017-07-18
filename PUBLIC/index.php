3d0b523568f9e65daea4fe770645377bdd52ef22
immerpro <proyecto.medd@gmail.com>
2017-07-18 15:36:58 -0500

    AUTOCARGA
    realize el codigo de la autocarga

--- PUBLIC/index.php
+++ PUBLIC/index.php
@@ -1,8 +1,20 @@
-<?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<?php
+//directorio del proyecto
+define("RUTAPROYECTO", dirname(__DIR__));
+
+//directorio app
+define("RUTAAPP", PROJECTPATH . '/APP');
+
+//autoload con namespaces
+function autoload_classes($class_name)
+{
+    $filename = PROJECTPATH . '/' . str_replace('\\', '/', $class_name) .'.php';
+    if(is_file($filename))
+    {
+        include_once $filename;
+    }
+}
+//registramos el autoload autoload_classes
+spl_autoload_register('autoload_classes');
 
